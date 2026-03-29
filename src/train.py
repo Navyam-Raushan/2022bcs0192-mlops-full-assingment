9114bad39e66fd704e97dfbe91581ab1b3218013
Raushan Kumar <navyamraushan@gmail.com>
2026-03-29 18:22:06 +0530

    fixed error in train.py

--- src/train.py
+++ src/train.py
@@ -8,10 +8,12 @@ import joblib
 import os
 import argparse
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("file:./mlruns")
 
 NAME = "Raushan Kumar"
 ROLL = "2022bcs0192"
+
 
 mlflow.set_experiment(f"{ROLL}_experiment")
 
